dafc22436698217c8eedc589238ff5912d02e6e4
VenkataKrishna Maridu <venkatakrishna_maridu@epam.com>
2025-07-08 05:33:11 +0530

    files added

---
+++ EPAM Interview questions/Practical Csharp and core with VS/CSharp/ExplicitInterface.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    public interface ILifeInsurance
+    {
+        void CalculatePremium();
+    }
+
+    public interface IPetInsurance
+    {
+        void CalculatePremium();
+    }
+    public class ExplicitInterface : ILifeInsurance, IPetInsurance
+    {
+        void ILifeInsurance.CalculatePremium()
+        {
+            Console.WriteLine("Calculation of LifeInsurance");
+        }
+
+        void IPetInsurance.CalculatePremium()
+        {
+            Console.WriteLine("Calculation of PetInsurance");
+        }
+
+        public void PrintPremiums()
+        {
+            ILifeInsurance policy = new ExplicitInterface();
+            policy.CalculatePremium();
+            IPetInsurance petPolicy = new ExplicitInterface();
+            petPolicy.CalculatePremium();
+        }
+    }
+}
---
+++ EPAM Interview questions/Practical Csharp and core with VS/CSharp/MultipleInheritence.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+
+    public class LifeInsurence : ILifeInsurance
+    {
+        public void CalculatePremium()
+        {
+            Console.WriteLine("Calclate Life Insurance");
+        }
+    }
+
+    public class PetInsurance : IPetInsurance
+    {
+        public void CalculatePremium()
+        {
+            Console.WriteLine("Calclate Pet Insurance");
+        }
+    }
+    internal class MultipleInheritence : ILifeInsurance, IPetInsurance
+    {
+        LifeInsurence lifeInsurence = new LifeInsurence();
+        PetInsurance petInsurance = new PetInsurance();
+        void ILifeInsurance.CalculatePremium()
+        {
+            lifeInsurence.CalculatePremium();
+        }
+
+        void IPetInsurance.CalculatePremium()
+        {
+            petInsurance.CalculatePremium();
+        }
+
+        public void PrintPremium()
+        {
+            ILifeInsurance lifeInsurance = new MultipleInheritence();
+            lifeInsurance.CalculatePremium();
+            IPetInsurance petInsurance = new MultipleInheritence();
+            petInsurance.CalculatePremium();
+        }
+    }
+}
